e3bafdd03abc021b63f8d3d3c6d3480dd92c342c
IrvinLab <48688466+IrvinLab@users.noreply.github.com>
2020-04-20 11:41:46 +0600

    Add files via upload

--- server.py
+++ server.py
@@ -2,6 +2,9 @@
 from socket import *
 import time, sys, random, math
 
+import pygame # TMP
+import pygame.freetype
+
 world = []
 
 mapa = sys.argv[1]
@@ -9,6 +12,16 @@ gamers = sys.argv[2]
 positive = sys.argv[3]
 negative = sys.argv[4]
 bots = sys.argv[5]
+
+x = 0
+y = 0
+
+sc = pygame.display.set_mode((0, 0), pygame.FULLSCREEN) # Полноэкранный режим
+pygame.display.set_caption("World of Shamanic")
+clock = pygame.time.Clock()
+pygame.init()
+
+#pygame.draw.circle(sc, (0,255,0), (200, 100), 0, 0)
 
 def main():
   print ("Server Shamanica ver 0.1") 
@@ -20,7 +33,7 @@ def main():
   createWorld(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5])
   
 def createWorld(ma, ga, po, ne, bo):
-    global world
+    global world, x ,y
     world.clear()
     print ("Creating...")
     ma = int(ma)
@@ -42,16 +55,38 @@ def createWorld(ma, ga, po, ne, bo):
         raise SystemExit(1)
     
     midle = ma / 2
-    otklonenie = int(random.random()*1500)
-    midle = int((midle-750) + otklonenie) # Координата центра материка
-    midleKord = int(math.sqrt(midle)/2)
+    ma = int(math.sqrt(ma)) # Грань карты x = y = ma
+    otklonenie = int(random.random()*2500)
+    midle = int((midle) + otklonenie) 
+    midleKord = int(math.sqrt(midle)/2) # Координата центра материка x = y = midleKord
+    print(midleKord)
     poluostrov = int(random.random()*20) # Колличество полуостровов
-    radius = int(int(random.random()*(ma/3))+ (ma/2)) # Радиус материка
+    radiusKord = int(int(random.random()*(ma/6))+ (ma/5)) # Радиус материка
+        
+    for m in range(ma):
+        for n in range(ma):
+            if x <= midleKord + radiusKord and x >= midleKord - radiusKord and y <= midleKord + radiusKord and y >= midleKord - radiusKord:
+                pygame.draw.circle(sc, (0,255,0), (x, y), 0, 0)
+            else:
+                pygame.draw.circle(sc, (0,0,255), (x, y), 0, 0)  
+            x += 1                
+        y += 1
+        x = 0
+    
     
 if __name__ == '__main__':
   main()
 
-
+pygame.display.update()
+while True:
+    clock.tick(60)
+    for i in pygame.event.get():
+        if i.type == pygame.QUIT:
+            pygame.quit()
+        elif i.type == pygame.KEYDOWN:    
+            if i.key == pygame.K_ESCAPE: #ESC
+                pygame.quit()  
+                
 # Объекты ландшафта:
 # 0 - вода, 1 - земля, 2 - гора, 3 - дерево
 #
